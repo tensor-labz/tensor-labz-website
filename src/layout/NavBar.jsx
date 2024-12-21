7562528a32df6a935dde0d59f9df66e94a4d7197
yukatharsana <primedevintern3@gmail.com>
2024-12-21 22:41:36 +0530

    refactor(NavBar):navigation bar logo section start

--- src/layout/NavBar.jsx
+++ src/layout/NavBar.jsx
@@ -1,9 +1,28 @@
 import React, { memo } from 'react'
 import NavData from './../data/NavData';
+import { FaDumbbell } from 'react-icons/fa6';
 
 function NavBar() {
     return (
-        <div>Nav</div>
+        <>
+          <nav>
+             <div className='container'>
+            
+        {/* logo section */}
+        <div className="text-2xl flex items-center gap-2 font-bold py-8">
+            <FaDumbbell />
+            <p> Tensor</p>
+            <p>Lab</p>
+        </div>
+        {/* Menu section */}
+        {/* Icon section */}
+        {/* Mobile hamburger menu section */}
+    </div>
+    </nav>
+    {/* Mobile siderbar section */}
+        </>
+      
+       
     )
 }
 
