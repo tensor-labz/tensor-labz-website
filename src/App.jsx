d900c105a89999c8f21e6b45b477f74795221173
ThanuMahee12 <thanumahee440@gmail.com>
2024-12-21 20:48:00 +0530

    refactor(reset):project Reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import router from './functions/router';
 
 function App() {
-  // useEffect(() => {
-  //   // Disable right-click
-  //   const handleContextMenu = (e) => e.preventDefault();
 
-  //   // Disable copy
-  //   const handleCopy = (e) => e.preventDefault();
-    
-
-  //   // Add listeners
-  //   document.addEventListener('contextmenu', handleContextMenu);
-  //   document.addEventListener('copy', handleCopy);
-
-  //   // Cleanup listeners on unmount
-  //   return () => {
-  //     document.removeEventListener('contextmenu', handleContextMenu);
-  //     document.removeEventListener('copy', handleCopy);
-  //   };
-  // }, []);
   return (
    <RouterProvider router={router} />
   );
