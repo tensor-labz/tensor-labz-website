1da256755b94a82c51316d63e27d99840752287e
ThanuMahee12 <thanumahee440@gmail.com>
2024-12-31 07:44:50 +0530

    feat(prettier):added prettifier in the project
    BRAKING CHANGE: project reset and Format options enabled

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,52 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    ignores: ['dist'], // Ignore the 'dist' directory
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'], // Apply to TypeScript files
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 2020, // ECMAScript version
+      sourceType: 'module', // Use ES Modules
+      globals: {
+        ...globals.browser,
+      },
+      parser: '@typescript-eslint/parser', // Use the TypeScript parser
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true, // Enable JSX parsing
+        },
+      },
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      react, // React plugin
+      'react-hooks': reactHooks, // React hooks plugin
+      'react-refresh': reactRefresh, // React Refresh plugin
+      '@typescript-eslint': typescriptEslint, // TypeScript plugin
+      prettier, // Prettier plugin
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
+      ...react.configs.recommended.rules, // Use recommended React rules
+      ...reactHooks.configs.recommended.rules, // Use recommended React Hooks rules
+      ...typescriptEslint.configs.recommended.rules, // Use recommended TypeScript rules
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
-      ],
+      ], // React Refresh rule
+      'prettier/prettier': 'error', // Prettier formatting as ESLint errors
+    },
+    settings: {
+      react: {
+        version: 'detect', // Automatically detect React version
+      },
     },
   },
-)
+];
