787205b4116659b2286862a0b5133c38535ac94b
ThanuMahee12 <thanumahee440@gmail.com>
2025-01-02 17:40:49 +0530

    fix(build):build error handling

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,4 +13,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'), // Alias @ to src directory
     },
   },
-})
+});
