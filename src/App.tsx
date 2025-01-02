e34de6c972d0eba3339d9f130fef66f2bbbf70de
ThanuMahee12 <thanumahee440@gmail.com>
2025-01-02 19:50:30 +0530

    style(tailwindcss):create cutom styles with utlities

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ function App() {
 
   return (
     <>
-     <h1 className='bg-red-500'>hhgfhfgfg</h1>
+     <h1 className='h1'>hhgfhfgfg</h1>
+     <h1 className='h2'>hhgfhfgfg</h1>
+     <h1 className='h3'>hhgfhfgfg</h1>
+     <h1 className='h4'>hhgfhfgfg</h1>
+     <h1 className='h5'>hhgfhfgfg</h1>
+     <h1 className='h6'>hhgfhfgfg</h1>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,24 @@ export default {
   ],
   theme: {
     extend: {
+      colors:{
+        primary:{
+          100: '#F0F0E4',
+          200: '#C2C2A3',
+          300: '#A3A380',
+          400: '#858566',
+          500: '#66664D',
+          600: '#4D4D33',
+          700: '#3F3F2E',
+          800: '#333326',
+          900: '#26261A',
+          950: '#030836',
+        },
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+      },
+    },
       container:{
         center: true,
         padding: {
