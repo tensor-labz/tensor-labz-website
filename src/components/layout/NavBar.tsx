96c2803caa60f7b0b033005583d5fa90d69120f8
Thanujan Mahendran <thanujan.tech@gmail.com>
2025-06-26 08:49:39 +0000

    Merged in hotfix/navbar (pull request #2)
    NavBar Optimization
    
    * pref(OfflineWarning):Offlinewarning Componnents optimization
    
    * pref(App.tsx):Optimize the App.tsx page with moved to Layout Component in to AnimatePresence
    
    * pref(NavItem):create NavItem componenet to optimize the page performance
    
    * fix(NavItem):NavItem uncomment AnimatePresence
    
    * pref(NavItem):navItem Icons Optimization Preofessional Removed Animations
    
    * pref(useScroll):Optimzed useScroll Hook and Performance

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,13 +1,11 @@
 import navData from '../../data/nav_data';
-import { NavLink, useLocation } from 'react-router-dom';
 import {AnimatePresence,motion } from 'framer-motion';
-import useScroll from '../../base/hooks/useScroll';
-import { useMemo } from 'react';
+import NavItem from './NavItem';
 
 const NavBar: React.FC = () => {
-  const isVisible = useScroll();
-  const {pathname}=useLocation()
-const isattack=useMemo(()=>pathname==='/services' || pathname==='/contact-us',[pathname])
+
+
+
   return (
     <nav className="md:flex hidden items-center space-x-6">
       <AnimatePresence>
@@ -23,32 +21,11 @@ const isattack=useMemo(()=>pathname==='/services' || pathname==='/contact-us',[p
               stiffness: 300
             }}
           >
-            <NavLink
+            <NavItem
+              nav={navItem.nav}
               to={navItem.to}
-              className="group relative inline-block overflow-hidden"
-            >
-              {isVisible ? (
-                <motion.span
-                  className={`file:inline-block text-lg font-semibold text-blue-800 relative  after:content-['']
-  after:rounded-xl
-  after:absolute
-  after:left-0 after:bottom-0
-  after:h-0.5 sm:after:h-1 after:w-0
-  after:bg-blue-800
-  after:transition-all after:duration-300 hover:after:w-3/4 `}
-                >
-                  {navItem.nav}
-                </motion.span>
-              ) : (
-                <motion.span
-                    className={`block p-2 text-xl ${isattack?"text-white":"text-blue-900 hover:bg-gradient-to-b from-white via-blue-50 to-blue-200"}  rounded-full`}
-                  whileHover={{ scale: 1.1, rotate: 5 }}
-                  whileTap={{ scale: 0.95 }}
-                >
-                  {navItem.icon}
-                </motion.span>
-              )}
-            </NavLink>
+              icon={navItem.icon} />
+
           </motion.div>
         ))}
       </AnimatePresence>
