c2dbab92153f6dadcee36a3c6a70ffa70bc7e1fb
ThanuMahee12 <thanujan.tech@gmail.com>
2025-07-03 02:41:24 +0530

    chroe(Aboutus.tsx):create glow affect for Aboutus Contents

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -38,7 +38,7 @@ const AboutUs: React.FC = memo(() => {
         <Section className="relative z-10 flex items-center justify-center h-full">
           {/* Overlay */}
           <div className="absolute inset-0 bg-gradient-to-b from-blue-200/50 via-white/80 to-blue-200/50 z-0" />
-          <div className="relative  mx-auto text-center px-6 sm:mt-0 mt-8">
+          <div className="w-11/12 relative  mx-auto text-center p-8 sm:mt-0 mt-8 rounded-2xl shadow-2xl backdrop-blur-sm">
             <h1 className="text-4xl md:text-6xl font-bold mb-6 text-blue-900 shadow-sm">
               {Data?.aboutus?.title?? "About Us"}
             </h1>
@@ -47,7 +47,7 @@ const AboutUs: React.FC = memo(() => {
             </p>
 
             {/* Section Items */}
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 text-center">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 text-center ">
               {isLoading ? (Array(3).fill("").map((_, i) => (<Placeholder key={ i} />))):aboutus_data?.map((section:any, index:number) => (
                 <AboutUsSectionItem
                   key={index}
