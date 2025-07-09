f7e9d33f9ed4b2b973682eb161ff68f890ea3d0a
ThanuMahee12 <thanujan.tech@gmail.com>
2025-07-09 13:01:08 +0530

    style(Contactus.tsx):responsive design colors and design

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -18,12 +18,12 @@ const ContactUs: React.FC = memo(() => {
           <source media="(min-width: 480px)" srcSet={Data?.contactus?.bg?.md} />
           <img src={Data?.contactus?.bg?.sm} alt="" className="w-full h-full object-cover" />
         </picture>
-        <div className="max-w-4xl w-full space-y-10 relative  p-8 rounded-2xl shadow-2xl backdrop-blur-sm mt-10 flex items-center justify-center flex-col">
+        <div className="max-w-4xl w-full space-y-10 relative  p-8 rounded-2xl shadow-2xl backdrop-blur-md mt-10 flex items-center justify-center flex-col">
 
           <motion.h1
             initial={{ opacity: 0, y: -50 }}
             animate={{ opacity: 1, y: 0 }}
-            className="md:text-5xl text-3xl font-bold text-center text-white md:mb-12 mb-6"
+            className="md:text-5xl text-3xl font-bold text-center md:text-blue-900 text-white md:mb-12 mb-6"
           >
             {Data?.contactus?.title || "Contact Us"}
           </motion.h1>
@@ -47,7 +47,7 @@ const ContactUs: React.FC = memo(() => {
             transition={{ delay: 0.5 }}
             className="text-center"
           >
-            <p className="text-white italic text-base md:text-xl max-w-2xl mx-auto font-light">
+            <p className="text-white sm:text-grey-600 italic text-sm md:text-xl max-w-2xl mx-auto sm:font-light font-bold">
               {Data?.contactus?.quote}
             </p>
           </motion.div>
