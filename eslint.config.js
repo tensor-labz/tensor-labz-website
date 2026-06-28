cce9ab5a08ea000a17df27e735621a73366a16c0
Thanujan Mahendran <thanujan.tech@gmail.com>
2026-06-28 11:42:40 -0400

    chore: vendor docs and image-lambda as submodules (#35)

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,7 @@ import prettierConfig from 'eslint-config-prettier';
 
 export default [
   {
-    ignores: ['dist'], // Ignore the 'dist' directory
+    ignores: ['dist', 'docs', 'image-lambda'], // Ignore build output and vendored submodules
   },
   {
     files: ['**/*.ts', '**/*.tsx'], // Apply to TypeScript files
